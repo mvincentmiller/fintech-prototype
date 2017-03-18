31a92f3ba765e068e734f25f6179d11f80b99c07
M Vincent Miller <m.vincent83@gmail.com>
2017-03-17 21:52:01 -0500

    hide the keys

--- app.js
+++ app.js
@@ -9,40 +9,40 @@ const app = new Koa();
 
 const quandlAPIkey = keys.secrets.quandl_api_key;
 
-// //cattle futures, CME Group
-// const commodity = 'CHRIS/CME_LC1';
-//
-// app.use(serve({rootDir: 'public'}))
-//
-// app.use(views(__dirname + '/views', { extension: 'ejs' }))
-//
-// router.get('/quandl', function *() {
-//
-//      let options = {
-//        url: 'http://www.quandl.com/api/v3/datasets/' + commodity + '/data.json?api_key='+quandlAPIkey+'&start_date=2017-01-01',
-//      };
-//
-//      let response = yield request(options);
-//      let parsed = JSON.parse(response.body);
-//      yield this.render('quandl', {names: parsed.dataset_data.column_names, data: parsed.dataset_data.data})
-//     });
-//
-//     router.get('/', function *() {
-//
-//      let options = {
-//        url: 'http://www.quandl.com/api/v3/datasets/' + commodity + '/data.json?api_key='+quandlAPIkey+'&start_date=2017-01-01',
-//      };
-//
-//      let response = yield request(options);
-//      let parsed = JSON.parse(response.body);
-//      yield this.render('index', {commodity: commodity, names: parsed.dataset_data.column_names, data: parsed.dataset_data.data})
-//
-//     });
-//
-// app
-//   .use(router.routes())
-//   .use(router.allowedMethods());
-//
-// app.listen(3000)
-//
-// console.log('listening on port 3000')
+//cattle futures, CME Group
+const commodity = 'CHRIS/CME_LC1';
+
+app.use(serve({rootDir: 'public'}))
+
+app.use(views(__dirname + '/views', { extension: 'ejs' }))
+
+router.get('/quandl', function *() {
+
+     let options = {
+       url: 'http://www.quandl.com/api/v3/datasets/' + commodity + '/data.json?api_key='+quandlAPIkey+'&start_date=2017-01-01',
+     };
+
+     let response = yield request(options);
+     let parsed = JSON.parse(response.body);
+     yield this.render('quandl', {names: parsed.dataset_data.column_names, data: parsed.dataset_data.data})
+    });
+
+    router.get('/', function *() {
+
+     let options = {
+       url: 'http://www.quandl.com/api/v3/datasets/' + commodity + '/data.json?api_key='+quandlAPIkey+'&start_date=2017-01-01',
+     };
+
+     let response = yield request(options);
+     let parsed = JSON.parse(response.body);
+     yield this.render('index', {commodity: commodity, names: parsed.dataset_data.column_names, data: parsed.dataset_data.data})
+
+    });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3000)
+
+console.log('listening on port 3000')
